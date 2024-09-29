ae9507956d8e134ff7a3dfd50db0898b123ee034
Рысдавлетов Темирлан Дауренович <t.rysdavletov@vlife.kz>
2024-09-29 15:25:14 +0500

    feat: add search functionality to header component
    - Integrated `useGetPosts` hook into the `Header` component for real-time search functionality.
    - Added controlled `Input` component to capture user input and trigger search requests.
    - Search queries are passed to the `fetchPosts` function, fetching relevant posts as the user types.
    - Linked the loading state to the `Input` to provide feedback during search operations.

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -3,15 +3,31 @@ import { headerStyles } from "./styles.ts";
 import { assetsConstant } from "../../../assets/assetsConstant.ts";
 import { Input } from "../input/Input.tsx";
 import { Profile } from "../../layout/profile/Profile.tsx";
+import { useGetPosts } from "../../../services/useGetPosts/useGetPosts.ts";
+import { useState } from "react";
+import { queryHandler } from "../../../utils/queryHandler/queryHandler.ts";
 
 export const Header: React.FC = () => {
+
+  const { loading, fetchPosts } = useGetPosts();
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchQuery(value);
+    queryHandler(searchQuery, 1)
+    fetchPosts(searchQuery, 1).then(r => r)
+  };
+
+
   return (
     <header style={headerStyles.header}>
       <img style={headerStyles.logo} src={assetsConstant.LOGO} alt="" />
       <Input
+        loading={loading}
         placeholder={"Batman"}
         maxWidth={"280px"}
-        handleInputChange={() => {}}
+        handleInputChange={handleInputChange}
       />
       <Profile username={"Your Name"} />
     </header>
